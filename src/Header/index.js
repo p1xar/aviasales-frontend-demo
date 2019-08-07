8a57205518b736a35c951c16a4a6c3858b03b367
p1xar <melnichukr@ya.ru>
2019-08-07 23:21:02 +0300

    Add header component

--- src/Header/index.js
+++ src/Header/index.js
@@ -1,42 +1,232 @@
 import React from 'react';
 import styled from 'styled-components';
-import logo from './logo.svg'
+import logo from './logo.svg';
+import arrow from './arrow.svg';
+import date from './date.svg';
+import shape from './shape.svg';
+import plane from './plane.svg';
+import { NavLink } from 'react-router-dom';
 
 const Header = styled.div`
     background: linear-gradient(151.09deg, #00B0DE 0.36%, #01AEDC 4.04%, #02ABDB 7.77%, #02ABDB 11.48%, #02ABDB 11.7%, #196EBD 100%);
 `;
 
 const Logo = styled.img`
-    width: 30px;
-    height: 29px;
     padding-left: 6.625rem;
     padding-top: 1.1875rem;
-    @media screen and (max-width: 768px) {
-        padding-left: 8px;
+    @media screen and (max-width: 48rem) {
+        padding-left: 0.5rem;
     }
 `;
-const LogoText = styled.p`
+
+const LogoText = styled(NavLink)`
     display: none;
     @media screen and (min-width: 48rem) {
         display: block;
         font-style: normal;
         font-weight: 300;
-        font-size: 20px;
+        font-size: 1.25rem;
         position: absolute;
-        left: 152px;
-        top: 0px;
-        line-height: 25px;
+        left: 9.5rem;
+        top: 1.25rem;
+        line-height: 1.5625rem;
         color: #fff;
+        text-decoration: none;
     }
-    
+    @media screen and (min-width: 20.0625rem) and (max-width: 48rem) {
+        display: block;
+        font-style: normal;
+        font-weight: 300;
+        font-size: 1.25rem;
+        position: absolute;
+        left: 3.125rem;
+        top: 1.25rem;
+        line-height: 1.5625rem;
+        color: #fff;
+        text-decoration: none;
+    }   
 `;
+
+const HeaderContent = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-top: 15.875rem;
+    @media screen and (max-width: 20rem) {
+        margin-top: 2rem;
+    }
+    @media screen and (min-width: 20.0625rem) and (max-width: 48rem) {
+        margin-top: 2.625rem;
+    }
+`;
+
+const HeaderTitle = styled.h1`
+    font-style: normal;
+    font-weight: 500;
+    font-size: 2.5rem;
+    line-height: 0.625rem;
+    color: #fff;
+    text-align: center;
+    @media screen and (max-width: 20rem) {
+        font-size: 1.25rem;
+        line-height: 1.4375rem;
+        padding: 0 0.375rem;
+    }
+    @media screen and (min-width: 20.0625rem) and (max-width: 48rem) {
+        font-size: 2rem;
+        line-height: 3rem;
+    }
+`;
+
+const HeaderTitleForm = styled.h2`
+    font-style: normal;
+    font-weight: bold;
+    font-size: 1.5rem;
+    line-height: 0.625rem;
+    color: #fff;
+    text-align: center;
+    @media screen and (max-width: 20rem) {
+        display: none;
+    }
+    @media screen and (min-width: 20.0625rem) and (max-width: 48rem) {
+        font-size: 1.25rem;
+        line-height: 1.4375rem;
+    }
+`;
+
+const FormWrapper = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: center; 
+    align-items: center;
+    flex-wrap: wrap;
+    margin-top: 2.5rem;
+    margin-bottom: 15.875rem;
+`;
+
+const FormContainer = styled.div`
+    display: inline;
+    position: relative;
+`;
+const StyledInput = styled.input`
+    color: #4A4A4A;
+    background: #fff;
+    border: none;
+    height: 3rem;
+    padding-left: 1rem;
+    margin-right: 0.2rem;
+    &:placeholder {
+        color: #A0B0B9;
+    }
+`;
+const FromInput = styled(StyledInput)`
+    max-width: 14rem;
+    height: 3rem;
+    border-radius: .3rem 0 0 .3rem;
+    color: #4A4A4A;
+    position: relative;
+`;
+const Icon = styled.img`
+    position: absolute;
+    width: 1rem;
+    height: 1.125rem;
+    right: 1rem;
+    top: 1rem;
+`;
+
+const ArrowIcon = styled(Icon)``;
+
+const ToInput = styled(StyledInput)`
+    max-width: 14.125rem;
+`;
+
+const DatePickerTo = styled(StyledInput)`
+    max-width: 11.25rem;
+`;
+
+const DatePickerBack = styled(StyledInput)`
+    max-width: 11.25rem;
+`;
+
+const Passangers = styled(StyledInput)`
+    max-width: 13.25rem;
+    border-radius: 0 0.3rem 0.3rem 0;
+`;
+
+const DateIcon = styled(Icon)``;
+
+const ShapeIcon = styled(Icon)`
+    width: 0.625rem;
+`;
+
+const CustomFormContainer = styled(FormContainer)`
+    flex-basis: 100%;
+    display: inline-flex;
+    justify-content: center;
+    margin-top: 3rem;
+`;
+
+const SearchButton = styled.a`
+    background: #FF9241;
+    color: white;
+    border: none;
+    border-radius: 0.25rem;
+    font-style: normal;
+    font-weight: 600;
+    font-size: 1.75rem;
+    line-height: 2.0625rem;
+    max-width: 19.25rem;
+    padding-left: 2.8125rem;
+    padding-top: 0.9375rem;
+    padding-bottom: 1rem;
+    padding-right: 4.625rem;
+    text-decoration: none; 
+`;
+
+const PlaneIcon = styled(Icon)`
+    width: 1.585rem;
+    height: 1.305rem;
+    top: 1.4rem;
+    margin-left: 15rem;
+    right: initial;
+`;
+
 export default () => {
     return (
         <Header>
             <Logo alt='Logo' src={logo} />
-            <LogoText>
-                    aviasales
-            </LogoText>
+            <LogoText to="/">aviasales</LogoText> 
+            <HeaderContent>
+                <HeaderTitle>Поиск дешевых авиабилетов</HeaderTitle>
+                <HeaderTitleForm>Лучший способ купить авиабилеты дешево</HeaderTitleForm>
+                <FormWrapper>
+                    <FormContainer>
+                        <FromInput placeholder="Город отправления"/>
+                        <ArrowIcon alt='Arrow' src={arrow} />
+                    </FormContainer>
+                   <FormContainer>
+                        <ToInput placeholder="Город прибытия"/>
+                    </FormContainer>
+                    <FormContainer>
+                        <DatePickerTo placeholder="Туда" />
+                    </FormContainer>
+                    <FormContainer>
+                        <DatePickerBack placeholder="Обратно" />
+                        <DateIcon alt="Date" src={date} />
+                    </FormContainer>
+                    <FormContainer>
+                        <Passangers placeholder="1 пассажир, эконом"readOnly/>
+                        <ShapeIcon alt="Triangle" src={shape} />
+                    </FormContainer>
+                    <CustomFormContainer>
+                        <SearchButton href="/">
+                            Найти билеты
+                        </SearchButton>
+                        <PlaneIcon alt="Plane" src={plane} />   
+                    </CustomFormContainer>   
+                </FormWrapper>
+            </HeaderContent>      
         </Header>
     )
 }
