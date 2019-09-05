77fb777898333b15177e13a132a70a1a75735984
p1xar <melnichukr@ya.ru>
2019-09-05 21:54:46 +0300

    Add elements to Dates component

--- src/Header/Inputs/Dates.js
+++ src/Header/Inputs/Dates.js
@@ -1,75 +1,84 @@
 import React from "react";
-import DayPicker, { DateUtils } from "react-day-picker";
-import DayPickerInput from "react-day-picker/DayPickerInput";
+import DayPickerInput, { LocaleUtils } from "react-day-picker/DayPickerInput";
 import "react-day-picker/lib/style.css";
+import { formatDate, parseDate } from "react-day-picker/moment";
+import moment from "moment";
+import MomentLocaleUtils from "react-day-picker/moment";
+import "moment/locale/ru";
+import "./style.css";
 
 export default class Calendar extends React.Component {
   constructor(props) {
     super(props);
-    this.handleDayClick = this.handleDayClick.bind(this);
-    this.handleDayMouseEnter = this.handleDayMouseEnter.bind(this);
-    this.state = this.getInitialState();
-  }
-
-  getInitialState() {
-    return {
-      from: null,
-      to: null,
-      enteredTo: null
+    this.handleFromChange = this.handleFromChange.bind(this);
+    this.handleToChange = this.handleToChange.bind(this);
+    this.state = {
+      from: undefined,
+      to: undefined
     };
   }
 
-  isSelectingFirstDay(from, to, day) {
-    const isBeforeFirstDay = from && DateUtils.isDayBefore(day, from);
-    const isRangeSelected = from && to;
-    return !from || isBeforeFirstDay || isRangeSelected;
-  }
-
-  handleDayClick(day) {
+  showFromMonth() {
     const { from, to } = this.state;
-    if (from && to && day >= from && day <= to) {
-      this.handleResetClick();
+    if (!from) {
       return;
     }
-    if (this.isSelectingFirstDay(from, to, day)) {
-      this.setState({
-        from: day,
-        to: null,
-        enteredTo: null
-      });
-    } else {
-      this.setState({
-        to: day,
-        enteredTo: day
-      });
+    if (moment(to).diff(moment(from), "months") < 2) {
+      this.to.getDayPicker().showMonth(from);
     }
   }
 
-  handleDayMouseEnter(day) {
-    const { from, to } = this.state;
-    if (!this.isSelectingFirstDay(from, to, day)) {
-      this.setState({
-        enteredTo: day
-      });
-    }
+  handleFromChange(from) {
+    this.setState({
+      from
+    });
+  }
+
+  handleToChange(to) {
+    this.setState({ to }, this.showFromMonth);
   }
 
   render() {
-    const { from, to, enteredTo } = this.state;
-    const modifiers = { start: from, end: enteredTo };
-    const disabledDays = { before: this.state.from };
-    const selectedDays = [from, { from, to: enteredTo }];
+    const { from, to } = this.state;
+    const modifiers = { start: from, end: to };
     return (
       <React.Fragment>
-        <DayPicker
-          className="Range"
-          numberOfMonths={1}
-          fromMonth={from}
-          selectedDays={selectedDays}
-          disabledDays={disabledDays}
-          modifiers={modifiers}
-          onDayClick={this.handleDayClick}
-          onDayMouseEnter={this.handleDayMouseEnter}
+        <DayPickerInput
+          value={from}
+          placeholder="Туда"
+          format="LL"
+          formatDate={formatDate}
+          parseDate={parseDate}
+          dayPickerProps={{
+            selectedDays: [from, { from, to }],
+            disabledDays: { after: to },
+            toMonth: to,
+            modifiers,
+            numberOfMonths: 1,
+            localeUtils: MomentLocaleUtils,
+            locale: "Ru",
+            onDayClick: () => this.to.getInput().focus()
+          }}
+          onDayChange={this.handleFromChange}
+        />
+        <DayPickerInput
+          ref={el => (this.to = el)}
+          value={to}
+          placeholder="Обратно"
+          format="LL"
+          formatDate={formatDate}
+          parseDate={parseDate}
+          dayPickerProps={{
+            selectedDays: [from, { from, to }],
+            disabledDays: { before: from },
+            modifiers,
+            month: from,
+            localeUtils: MomentLocaleUtils,
+            locale: "Ru",
+            fromMonth: from,
+            numberOfMonths: 1
+          }}
+          onDayChange={this.handleToChange}
         />
       </React.Fragment>
     );
