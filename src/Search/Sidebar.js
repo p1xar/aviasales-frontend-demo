b74671329adbdf2b035ec749308a061133041bd9
p1xar <melnichukr@ya.ru>
2019-09-17 21:42:06 +0300

    Add handler for transfer section in Sidebar component

--- src/Search/Sidebar.js
+++ src/Search/Sidebar.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 import triangle from "./triangle.svg";
 import clear from "./clear.svg";
 
-const Sidebar = styled.div`
+const SidebarWrapper = styled.div`
   width: 18.75rem;
   background: lightgrey;
   border-radius: 0.25rem;
@@ -36,15 +36,25 @@ const Triangle = styled.img`
   width: 0.625rem;
   height: 0.3125rem;
   margin-right: 0.375rem;
+  transform: ${props => props.rotate || ""};
+  transition: 0.3s ease;
 `;
 
 const Clear = styled.img`
   width: 1rem;
   height: 1rem;
   margin-left: auto;
+  display: ${props => props.display || "block"};
 `;
 
-const SidebarItems = styled(SidebarSection)``;
+const SidebarItems = styled(SidebarSection)`
+  transition: all 0.4s linear;
+  transition-property: all;
+  transition-duration: 0.5s;
+  transition-timing-function: cubic-bezier(0, 1, 0.5, 1);
+  max-height: ${props => props.maxHeight || "18.75rem"};
+  overflow: hidden;
+`;
 
 const CheckBoxItem = styled(SidebarTitle)`
   height: 2.25rem;
@@ -67,37 +77,64 @@ const CheckBox = styled.div`
   margin-right: 0.375rem;
 `;
 
-export default () => {
-  return (
-    <React.Fragment>
-      <Sidebar>
-        <SidebarSection>
-          <SidebarTitle>
-            <Triangle alt="Triangle" src={triangle} />
-            <SidebarTitleText href="#">ПЕРЕСАДКИ</SidebarTitleText>
-            <Clear alt="Clear" src={clear} />
-          </SidebarTitle>
-          <SidebarItems>
-            <CheckBoxItem>
-              <CheckBox />
-              <CheckBoxLabel>Все</CheckBoxLabel>
-            </CheckBoxItem>
-            <CheckBoxItem>
-              <CheckBox />
-              <CheckBoxLabel>Без пересадок</CheckBoxLabel>
-            </CheckBoxItem>
-            <CheckBoxItem>
-              <CheckBox />
-              <CheckBoxLabel>2 пересадки</CheckBoxLabel>
-            </CheckBoxItem>
-            <CheckBoxItem>
-              <CheckBox />
-              <CheckBoxLabel>3 пересадки</CheckBoxLabel>
-            </CheckBoxItem>
-          </SidebarItems>
-        </SidebarSection>
-        <hr />
-      </Sidebar>
-    </React.Fragment>
-  );
-};
+class Sidebar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      transfer: true
+    };
+    this.handleTransferChange = this.handleTransferChange.bind(this);
+  }
+
+  handleTransferChange(event) {
+    event.preventDefault();
+    this.setState({ transfer: !this.state.transfer });
+  }
+  render() {
+    return (
+      <React.Fragment>
+        <SidebarWrapper>
+          <SidebarSection>
+            <SidebarTitle>
+              <Triangle
+                alt="Triangle"
+                src={triangle}
+                rotate={this.state.transfer ? "" : "rotate(180deg)"}
+              />
+              <SidebarTitleText href="#" onClick={this.handleTransferChange}>
+                ПЕРЕСАДКИ
+              </SidebarTitleText>
+              <Clear
+                alt="Clear"
+                src={clear}
+                display={this.state.transfer ? "flex" : "none"}
+                onClick={this.handleTransferChange}
+              />
+            </SidebarTitle>
+            <SidebarItems maxHeight={this.state.transfer ? "18.75rem" : "0rem"}>
+              <CheckBoxItem>
+                <CheckBox />
+                <CheckBoxLabel>Все</CheckBoxLabel>
+              </CheckBoxItem>
+              <CheckBoxItem>
+                <CheckBox />
+                <CheckBoxLabel>Без пересадок</CheckBoxLabel>
+              </CheckBoxItem>
+              <CheckBoxItem>
+                <CheckBox />
+                <CheckBoxLabel>2 пересадки</CheckBoxLabel>
+              </CheckBoxItem>
+              <CheckBoxItem>
+                <CheckBox />
+                <CheckBoxLabel>3 пересадки</CheckBoxLabel>
+              </CheckBoxItem>
+            </SidebarItems>
+          </SidebarSection>
+          <hr />
+        </SidebarWrapper>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Sidebar;
